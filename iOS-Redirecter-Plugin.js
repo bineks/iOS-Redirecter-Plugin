ae722ce3d9baf034e2c9a4fee9d85b6eb74a8675
Pavel Sheiman <pavel.sheiman@gmail.com>
2014-06-09 14:16:56 +0300

    added package.json

--- iOS-Redirecter-Plugin.js
+++ iOS-Redirecter-Plugin.js
@@ -1,20 +1,64 @@
-function iOSRedirect(variables){
-    var urlRegExp = /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i;
-    if(!urlRegExp.test(variables.redirectTo)){
-        throw new Error("iOS-Redirecter-Plugin - URL is not a valid one");
+"use strict";
+
+(function (ns) {
+  var InvalidArgumentException = function(message) {
+    this.message = message;
+
+    this.getMessage = function() {
+      return this.message;
     }
-    var checkArray = new Array();
-    if(variables.iPad){
-        checkArray.push('iPad');
+  }
+
+  InvalidArgumentException.prototype.toString = function () { return 'InvalidArgumentException: ' + this.message; }
+
+  var defaults = {
+    urlValidationRegExpr: /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
+    iPhone: true,
+    iPad: true
+  };
+
+  var pluginConfig = {};
+
+  ns.initialize = function (config) {
+    if (!config.hasOwnProperty('redirectTo')) throw new InvalidArgumentException('Config property redirectTo is not defined.');
+    pluginConfig = mergeObjects(config, defaults);
+
+    if (!pluginConfig.urlValidationRegExpr.test(pluginConfig.redirectTo)) {
+      throw new InvalidArgumentException('Provided URL is not valid. Please provide valid URL for redirectTo property.');
     }
-    if(variables.iPhone){
-        checkArray.push('iPhone');
+
+    return this;
+  };
+
+  ns.redirect = function() {
+    var userAgents = [];
+
+    if(pluginConfig.iPhone == true) userAgents.push('iPhone');
+    if(pluginConfig.iPad == true) userAgents.push('iPad');
+
+    if(userAgents.length >= 1) {
+      var userAgentsRegExpr = new RegExp(userAgents.join('|'), 'g');
+      if (userAgentsRegExpr.test(navigator.userAgent)) {
+        location.href = pluginConfig.redirectTo;
+      }
     }
-    var checkString = checkArray.join('|');
-    if(checkString){
-        var checkRegExp = new RegExp(checkString,'g');
-        if(checkRegExp.test(navigator.userAgent)){
-            location.href = variables.redirectTo;
-        }
+
+    return this;
+  };
+
+  var mergeObjects = function (objectA, objectB) {
+    var objectC = {}, attributeName;
+
+    for (attributeName in objectA) {
+      if(objectA.hasOwnProperty(attributeName)) objectC[attributeName] = objectA[attributeName];
     }
-}
+
+    for (attributeName in objectB) {
+      if(objectB.hasOwnProperty(attributeName)) objectC[attributeName] = objectB[attributeName];
+    }
+
+    return objectC;
+  };
+}(window.iOSRedirecter = window.iOSRedirecter || {}))
+
+
